0168a3b56972bb6c41fdda490176203b61917013
田中 哉太 <t.kanata@mac0021.np.co.jp>
2021-02-09 14:48:51 +0900

    フォームからの値をフォームの下に表示。アルゴリズム実装前。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,13 @@
 class PostsController < ApplicationController
   def new
-    #@post = Post.destroy_all
     @post = Post.new
-    #@post = Post.new(card_info: "S1 S2 S3 S4 S5")
-    #@n = 0
   end
 
-  def create
-    #@post = Post.create(card_info: "S1 S2 S3 S4 S5")
-    #@post = Post.create(post_params)
+  def result
     @post = Post.new(post_params)
-    #if @post.find(1)
-    #@post.find(1).destroy
-    #end
-    #@n += 1
-    #役の判定しようね
-    redirect_to("/show")
   end
 
-  def show
-    @post = params[:card_info]
-  end
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   root "posts#new"
   get  "/" => "posts#new"
-  #post "/" => "posts#create"
-  #patch "/" => "posts#create"
-  post "/show" => "posts#show"
-  #patch "/show" => "posts#create"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # app/api/api.rbをマウント
-  # mount API::Base => '/'
+  post "/result" => "posts#result"
+
 end
