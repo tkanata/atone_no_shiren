b70c8413ac974969e2cf6bf6eefeb644b92aa34d
田中 哉太 <t.kanata@mac0021.np.co.jp>
2021-03-10 10:14:58 +0900

    modified how_many_card(record) method

--- app/models/poker.rb
+++ app/models/poker.rb
@@ -2,21 +2,26 @@
 class PokerValidator < ActiveModel::Validator
   def validate(record)
     if record.card_info.nil?
+      # nilだった場合のエラーメッセージは半角スペースに関するメッセージと同じ
       record.errors[:card_info] << Settings.ERROR_MESSAGE.HALF_WIDTH_SPACE
 
     elsif record.card_info.empty?
       record.errors[:card_info] << Settings.ERROR_MESSAGE.EMPTY
 
     else
+      # 5つの単語が半角スペース区切りで入力されているかを確認する
       unless record.card_info.match(Settings.REGEX.HALF_WIDTH_SPACE)
         record.errors[:card_info] << Settings.ERROR_MESSAGE.HALF_WIDTH_SPACE
         # 5枚以上のカードが入力されたら枚数を表示するバリデーション
         how_many_card(record)
       end
 
+      # 重複するカードがあるか確認
       record.errors[:card_info] << Settings.ERROR_MESSAGE.DUPLICATE if record.card_info.match(Settings.REGEX.DUPLICATED)
 
+      # 初期化
       invalid = false
+      # カードを1枚ずつ確認
       record.card_info.split(' ').each_with_index do |card, index|
         if card !~ Settings.REGEX.EACH_CARD
           record.errors[:card_info] << "#{index + 1}枚目のカード情報が適切ではありません。(#{card})"
@@ -24,6 +29,7 @@ class PokerValidator < ActiveModel::Validator
         end
       end
 
+      # いずれかのカードが不正の場合に最後にこのエラーメッセージを表示する
       record.errors[:card_info] << Settings.ERROR_MESSAGE.VALID_SUIT_NUM if invalid == true
 
     end
@@ -32,11 +38,9 @@ class PokerValidator < ActiveModel::Validator
   private
 
   def how_many_card(record)
-    if record.card_info.split(' ').count > 5
-      record.errors[:card_info] << "カードが#{record.card_info.split(' ').count}枚あります。"
-    end
+    num_of_cards = record.card_info.split(' ').count
+    record.errors[:card_info] << "カードが#{num_of_cards}枚あります。" if num_of_cards > 5
   end
-
 end
 
 # モデルではバリデーションを行う
